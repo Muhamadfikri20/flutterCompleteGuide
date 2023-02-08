8797cabe27436a005e0a2454d6dd16755c357100
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-02-08 15:28:24 +0700

    [ADD]constructors and named Arguments

---
+++ latihan/dart_constructors_namedArguments.dart
@@ -0,0 +1,24 @@
+class Person {
+  String? name;
+  int age;
+  Person({this.name, this.age = 30});
+}
+
+double addNumbers(double num1, double num2) {
+// use num as a type because it should work with int and double
+  return num1 + num2;
+}
+
+void main() {
+  var p1 = Person(name: 'Max');
+  var p2 = Person(age: 31, name: 'Manu');
+  p2.name = 'Manu';
+  print(p1.name);
+  print(p2.name);
+  double firstResult = addNumbers(1, 1);
+  print(firstResult + 1);
+
+  print(addNumbers(1, 2.6));
+  print('Hello!');
+}
+//run terminal : dart dart_constructors_namedArguments.dart
