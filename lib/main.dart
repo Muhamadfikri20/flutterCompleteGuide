ba8c65c8e487913c5251e1ff3d0fb724aa2e0722
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-02-14 15:59:41 +0700

    [UPD]using_private_properties

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+  var _questionIndex = 0;
 
   void answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
 
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   // This widget is the root of your application.
@@ -31,7 +31,7 @@ class _MyAppState extends State<MyApp> {
             ),
             body: Column(
               children: <Widget>[
-                Text(questions[questionIndex]),
+                Text(questions[_questionIndex]),
                 RaisedButton(
                   onPressed: answerQuestion,
                   child: Text('Answer 1'),
