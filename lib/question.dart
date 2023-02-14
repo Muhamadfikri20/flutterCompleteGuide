e8476eedb14b8066102387f02e4a088cac12cb20
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-02-14 16:24:48 +0700

    [upd]first_styling_and_layouting_steps

--- lib/question.dart
+++ lib/question.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
@@ -5,6 +7,14 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText!);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText!,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
