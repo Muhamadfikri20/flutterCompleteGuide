acd5c9c32d6597613340bda944fb80f0b1af0d85
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-03-04 11:15:47 +0700

    [UPD]resetting_the_quiz

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,12 @@ class _MyAppState extends State<MyApp> {
   ];
   var _questionIndex = 0;
   var _totalScore = 0;
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
   void _answerQuestion(int score) {
     _totalScore += score;
@@ -71,6 +77,6 @@ class _MyAppState extends State<MyApp> {
                     questionIndex: _questionIndex,
                     questions: questions,
                   )
-                : Result(_totalScore)));
+                : Result(_totalScore, _resetQuiz)));
   }
 }
